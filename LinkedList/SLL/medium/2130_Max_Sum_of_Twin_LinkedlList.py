6f68e01e525fdde37b1080e9813cb0b7fe6f6ca6
harshwadari <harshwadari@gmail.com>
2026-09-03 22:10:31 +0530

    solved some lc 75

--- LinkedList/SLL/medium/2130_Max_Sum_of_Twin_LinkedlList.py
+++ LinkedList/SLL/medium/2130_Max_Sum_of_Twin_LinkedlList.py
@@ -18,3 +18,60 @@ def pairSum(self, head):
         ans = max(ans,slow.val + stack.pop())
         slow = slow.next
     return ans       
+
+
+
+
+# Using array Appraoch 
+# TC = O(2N) and SC = O(N)
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def pairSum(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: int
+        """
+        result = []
+        temp = head
+        total = 0
+        ans = 0
+        while temp is not None:
+            result.append(temp.val)
+            temp = temp.next
+        n = len(result)
+        for i in range(len(result)):
+            total = result[i] + result[n - 1 - i]
+            ans = max(ans,total)
+        return ans
+
+
+
+# Optimal Approach 
+# TC = O(3N) and SC = O(1)
+
+def twinSumLinkedList(head):
+    slow = head
+    fast = head
+    while fast != None and fast.next != None:
+        slow = slow.next
+        fast = fast.next
+    prev = None
+    curr = slow
+    while curr != None:
+        nodenext = curr.next
+        curr.next = prev
+        prev = curr
+        curr = nodenext
+    first = head
+    second = prev
+    ans = 0
+    while second != None:
+        total = first.val + second.val
+        ans = max(ans,total)
+        first = first.next
+        second = second.next
+    return ans 
